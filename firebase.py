bc9208d1c02500d37a173206b199de295bcdddc1
alechkos <alechqos@gmail.com>
2021-12-02 16:11:32 +0200

    pep8

--- firebase.py
+++ firebase.py
@@ -1,16 +1,15 @@
 import pyrebase
 
-
 # configuration key
 firebaseConfig = {
-  'apiKey': "AIzaSyBCs37bq0qIIgyRC77AOBMKlwZdR6eqFNI",
-  'authDomain': "triperadvise.firebaseapp.com",
-  'projectId': "triperadvise",
-  'storageBucket': "triperadvise.appspot.com",
-  'messagingSenderId': "415829560302",
-  'appId': "1:415829560302:web:27d2770750748f92875cff",
-  'measurementId': "G-8SG87YPDT1",
-  'databaseURL': "https://triperadvise-default-rtdb.europe-west1.firebasedatabase.app/"
+    'apiKey': "AIzaSyBCs37bq0qIIgyRC77AOBMKlwZdR6eqFNI",
+    'authDomain': "triperadvise.firebaseapp.com",
+    'projectId': "triperadvise",
+    'storageBucket': "triperadvise.appspot.com",
+    'messagingSenderId': "415829560302",
+    'appId': "1:415829560302:web:27d2770750748f92875cff",
+    'measurementId': "G-8SG87YPDT1",
+    'databaseURL': "https://triperadvise-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 # Firebase Authentication
@@ -20,4 +19,3 @@ auth = firebase.auth()
 # Databases
 db = firebase.database()
 storage = firebase.storage()
-
